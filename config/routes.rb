b326fab381c7cdc195d4d8a798a9c1f42be62fca
edo1881 <edo.gi00@hotmail.it>
2022-08-30 16:51:15 +0200

    chall_create_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users,controllers: {omniauth_callbacks: "users/omniauth_callbacks" }
   resources :contactform, only: [:create,:new]
-  resources :challenges,only: [:new,:index,:update,:show]
+  resources :challenges,only: [:new,:create,:index,:update,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  #post '/send_email' 'contactform#create' 
-  get '/challenges', to: 'challenges#index'
+  post '/send_email',to: 'contactform#create' 
+  #get '/challenges', to: 'challenges#index'
   get '/scoreboard', to: 'scoreboard#index'
   get '/profile', to: 'profile#index'
   
