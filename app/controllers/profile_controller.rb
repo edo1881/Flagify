b83161f1731b249d660378b6fa970113e2c6ccd7
thoms2901 <thomas.kirschner2901@gmail.com>
2022-09-04 21:03:18 +0200

    profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,20 @@
 class ProfileController < ApplicationController
+    before_action :authenticate_user!
     def index
         @user = User.all
+        @done_challenges = UserChallenge.all.where(:user_id => 1).where.not(:flag_timestamp => nil)
+        @hint_challenges = UserChallenge.all.where(:user_id => 1).where.not(:hint_timestamp => nil)
+
+        @n_challenge_done = @done_challenges.count
+        @n_hint_used = @hint_challenges.count
+
+        @score = 0
+        @done_challenges.each do |a|
+            @score += Challenge.find(a.challenge_id).score
+        end
+            
+        # done_challenges.each do |a|
+        #     Challenge.find(a.challenge_id)
+        # end
     end
 end
--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,4 +1,5 @@
 class ScoreboardController < ApplicationController
+    before_action :authenticate_user!
     def index
         @users = User.all
         
