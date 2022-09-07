f1586e6609866d22d4dd05d353e0c2c2bc836d7b
thoms2901 <thomas.kirschner2901@gmail.com>
2022-09-07 18:52:34 +0200

    profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,8 +2,8 @@ class ProfileController < ApplicationController
     before_action :authenticate_user!
     def index
         @user = User.all
-        @done_challenges = UserChallenge.all.where(:user_id => 1).where.not(:flag_timestamp => nil)
-        @hint_challenges = UserChallenge.all.where(:user_id => 1).where.not(:hint_timestamp => nil)
+        @done_challenges = UserChallenge.all.where(:user_id => current_user.id).where.not(:flag_timestamp => nil)
+        @hint_challenges = UserChallenge.all.where(:user_id => current_user.id).where.not(:hint_timestamp => nil)
 
         @n_challenge_done = @done_challenges.count
         @n_hint_used = @hint_challenges.count
