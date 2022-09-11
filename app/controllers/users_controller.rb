1cfa80fafff1d69c77925e2cb09288171fed0a4a
edo1881 <edo.gi00@hotmail.it>
2022-09-11 11:24:26 +0200

    profile_changed_to_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
-class ProfileController < ApplicationController
+class UsersController < ApplicationController
     before_action :authenticate_user!
-    def index
+    def def new
+        @user = User.new
+    end
+    
+ 
+    def show
         @user = User.all
         @done_challenges = UserChallenge.all.where(:user_id => current_user.id).where.not(:flag_timestamp => nil)
         @hint_challenges = UserChallenge.all.where(:user_id => current_user.id).where.not(:hint_timestamp => nil)
 
         @n_challenge_done = @done_challenges.count
-        @n_hint_used = @hint_challenges.count
-
+        @n_hint_used = 0
+        if @hint_challenges.count 
+            @n_hint_used = @hint_challenges.count  
+        end
         @score = 0
         @done_challenges.each do |a|
             @score += Challenge.find(a.challenge_id).score
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :users,controllers: {omniauth_callbacks: "users/omniauth_callbacks" }
   resources :contactform, only: [:create,:new]
   resources :challenges
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/challenges/:id/delete', to: 'challenges#destroy'
-  get '/profile/become_player', to: 'profile#become_player'
+  #get '/profile/become_player', to: 'profile#become_player'
   post '/send_email',to: 'contactform#create' 
   get '/scoreboard', to: 'scoreboard#index'
-  get '/profile', to: 'profile#index'
+ # get '/profile', to: 'profile#index'
   post '/challenges/check_flag', to: 'challenges#check_flag'
   
 end
